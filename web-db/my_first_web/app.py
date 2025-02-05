da466640dcd67ad95e654e2f7834369d9b9d7270
PetraZednikova <petra.domasinska@gmail.com>
2025-02-05 21:58:44 +0100

    s chybama

--- web-db/my_first_web/app.py
+++ web-db/my_first_web/app.py
@@ -1,20 +1,43 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from fake_db import users
+
 
 app = Flask(__name__)
 
 
 @app.route('/')    #decorater
 def home():
-    return render_template("/pages/home.html") # výstup
+    return render_template("pages/home.html") # výstup
 
 @app.route('/about')
 def about():
-    return render_template("/pages/about.html")
+    return render_template("pages/about.html")
 
 @app.route('/contact')
 def contact():
-    return render_template("/pages/contact.html")
+    return render_template("pages/contact.html")
 
+@app.route("/calc/<int:a>/<int:b>")
+def calculator(a, b):
+    #a = 10
+    #b = 20
+    return render_template("pages/calc.html", a=a, b=b, suma= a+b)
+
+@app.route("/users")
+def user_list():
+    return render_template("pages/users_list.html", users=users)
+
+@app.route("/users/<int:id>")
+def user_detail(id):
+    return render_template("pages/users_detail.html", user=users[id])
+
+@app.route("/mes", methods=["GET", "POST"])
+def chat():
+    if request.method == "POST":
+        message = request.form["message"]
+        print(message)
+
+    return render_template("/pages/chat.html")
 
 if __name__=="__main__":
     app.run(debug=True)
---
+++ web-db/my_first_web/fake_db.py
@@ -0,0 +1,12 @@
+users = [
+    {"id": 0, "name": "John Doe", "email": "john.doe@example.com", "is_logged_in": True},
+    {"id": 1, "name": "Jane Smith", "email": "jane.smith@example.com", "is_logged_in": False},
+    {"id": 2, "name": "Emily Davis", "email": "emily.davis@example.com", "is_logged_in": True},
+    {"id": 3, "name": "Michael Brown", "email": "michael.brown@example.com", "is_logged_in": False},
+    {"id": 4, "name": "Sophia Wilson", "email": "sophia.wilson@example.com", "is_logged_in": True},
+    {"id": 5, "name": "James Taylor", "email": "james.taylor@example.com", "is_logged_in": False},
+    {"id": 6, "name": "Olivia Martinez", "email": "olivia.martinez@example.com", "is_logged_in": True},
+    {"id": 7, "name": "Liam Johnson", "email": "liam.johnson@example.com", "is_logged_in": False},
+    {"id": 8, "name": "Ava Lee", "email": "ava.lee@example.com", "is_logged_in": True},
+    {"id": 9, "name": "William Garcia", "email": "william.garcia@example.com", "is_logged_in": False}
+]
